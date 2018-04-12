b1940d2584a85c40e5bfe983f14454931ec00043
XodTuza <gordie2u@gmail.com>
2018-04-12 20:52:16 +0100

    first commit

---
+++ stageone.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>StageOne</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="main-content row">
+      <aside class="left col-4">
+          <img id="hr1" src="hr.png" height="2px">
+      </aside>
+    <article class="center col-4">
+    	<p id="p1">Godwin Okoi</p>
+
+    	<p id="demo"><?php
+  
+  echo  date("l,d,m,y"). ".".  "   " . date("h:i:sa");
+
+  ?></p>
+    </article>
+	<article class="left col-4">
+    	<img id="hr2" src="hr.png" height="2px">
+    </article>
+    </div>
+	<footer>HotelNG Internship</footer>
+</body>
+</html>
